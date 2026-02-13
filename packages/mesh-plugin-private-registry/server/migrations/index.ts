b730be7b227f4dc29e881d71831eb5e1a4d542c9
Jonas Jesus <jonasdasilvajesus@outlook.com>
2026-02-13 10:42:16 -0300

    refactor(private-registry): consolidate migrations 002+003 into single 002

--- packages/mesh-plugin-private-registry/server/migrations/index.ts
+++ packages/mesh-plugin-private-registry/server/migrations/index.ts
@@ -1,10 +1,5 @@
 import type { ServerPluginMigration } from "@decocms/bindings/server-plugin";
 import { migration as migration001 } from "./001-private-registry";
 import { migration as migration002 } from "./002-publish-requests";
-import { migration as migration003 } from "./003-publish-api-keys";
 
-export const migrations: ServerPluginMigration[] = [
-  migration001,
-  migration002,
-  migration003,
-];
+export const migrations: ServerPluginMigration[] = [migration001, migration002];
