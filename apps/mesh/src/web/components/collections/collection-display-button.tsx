48cd7779c52331f58767de22c0942fe8a491859f
guitavano <tavano62@gmail.com>
2026-03-16 18:23:15 -0300

    fmt

--- apps/mesh/src/web/components/collections/collection-display-button.tsx
+++ apps/mesh/src/web/components/collections/collection-display-button.tsx
@@ -79,18 +79,19 @@ export function CollectionDisplayButton({
       </TooltipProvider>
       <DropdownMenuContent align="end" className="w-[200px] p-0 gap-0">
         {/* View Mode Toggle */}
-        {(onViewModeChange && viewMode) && <div className="p-2 border-b border-border">
-          <ViewModeToggle
-            value={viewMode}
-            onValueChange={onViewModeChange}
-            fullWidth
-            options={[
-              { value: "table", icon: <List /> },
-              { value: "cards", icon: <Grid01 /> },
-            ]}
-          />
-        </div>
-}
+        {onViewModeChange && viewMode && (
+          <div className="p-2 border-b border-border">
+            <ViewModeToggle
+              value={viewMode}
+              onValueChange={onViewModeChange}
+              fullWidth
+              options={[
+                { value: "table", icon: <List /> },
+                { value: "cards", icon: <Grid01 /> },
+              ]}
+            />
+          </div>
+        )}
         {/* Sort Options */}
         {sortOptions.length > 0 && onSort && (
           <div className="p-1 flex flex-col gap-0.5 border-b border-border">
